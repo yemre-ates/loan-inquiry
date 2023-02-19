6d02a916c7ad8fd68953d9bfc6ebfff0ca07be38
yemre-ates <yemre.ates@hotmail.com>
2023-02-19 15:12:23 +0300

    component scan update

--- src/main/java/com/yea/loaninquiry/LoanInquiryApplication.java
+++ src/main/java/com/yea/loaninquiry/LoanInquiryApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan("com.kocfinans.loaninquiry")
+@ComponentScan("com.yea.loaninquiry")
 @SpringBootApplication
 public class LoanInquiryApplication {
 
